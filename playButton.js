df534e7fb3922ba86e755402bb7f6a13abd7f46a
antonioii <asergiocosta.as@gmail.com>
2022-09-10 20:34:53 -0300

    Add the rock paper scissors choices after the play
    Create the buttons of choices after the user has clicked in play

--- playButton.js
+++ playButton.js
@@ -1,6 +1,37 @@
-const playButton = document.querySelector('.play');
+const buttonsDiv = document.querySelector('.buttons');
+let playButton = document.querySelector('.play');
 playButton.addEventListener('click', startChoices, {once:true});
 
 function startChoices(){
     document.querySelector('h1').textContent = "Make your choice";
+    //change the name of "Rock Paper Scisor" to "Make your choice"
+
+    buttonsDiv.removeChild(playButton);
+    //take off the play button
+
+    let rulesButton = document.querySelector('.rules');
+    //creat a node object for the rule button
+    
+    let choicesDiv = document.createElement('div');
+    choicesDiv.classList.add('choicesDiv');
+    //create a choices div to receive the rock, paper and scissors buttons
+
+        let rockButton = document.createElement('button');
+        rockButton.classList.add('rock');
+        rockButton.textContent = "ROCK";
+        choicesDiv.appendChild(rockButton);
+        //creates the rock button
+
+        let paperButton = document.createElement('button');
+        paperButton.classList.add('paper');
+        paperButton.textContent = "PAPER";
+        choicesDiv.appendChild(paperButton);
+        //creates the paper button
+
+        let scizButton = document.createElement('button');
+        scizButton.classList.add('sciz');
+        scizButton.textContent = "SCISSORS";
+        choicesDiv.appendChild(scizButton);
+        //creates the scissors button
+    buttonsDiv.insertBefore(choicesDiv, rulesButton);
 }
